e689f981c014956ff4f560ae0962390169b92600
Tone Alex Guerra <toneguerra@yahoo.com.br>
2021-05-23 14:40:12 -0300

    Componente TOAST adicionado

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -8,7 +9,7 @@ import { AlertController } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  constructor(public alertController: AlertController) {}
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
 
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
@@ -24,7 +25,7 @@ export class Tab1Page {
         }, {
           text: 'Sim, favoritar',
           handler: () => {
-            console.log('Confirm Okay');
+            this.apresentarToast();
           }
         }
       ]
@@ -33,4 +34,14 @@ export class Tab1Page {
     await alert.present();
   }
 
+
+  async apresentarToast() {
+    const toast = await this.toastController.create({
+      message: 'Filme adicionado aos favoritos.',
+      duration: 2000,
+      color: 'success',
+      position:'middle'
+    });
+    toast.present();
+  }
 }
