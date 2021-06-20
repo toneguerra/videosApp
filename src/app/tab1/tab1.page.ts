c711ccb8dc29cea1758810c54e0318330fc66219
Tone Alex Guerra <toneguerra@yahoo.com.br>
2021-06-19 21:10:15 -0300

    Adicionamos array de filmes e interface

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { IFilme } from '../models/iFilme.model';
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +9,27 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Videos App';
+
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Mortal Kombat (2021)',
+      lancamento: '15/04/2021',
+      duracao: '1h 50m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w220_and_h330_face/w8BVC3qrCWCiTHRz4Rft12dtQF0.jpg',
+      generos:['Ação', 'Fantasia', 'Aventura']
+    },
+    {
+      nome:'Quem Matou Sara? (2021)',
+      lancamento:'12/05/2021',
+      duracao:'40m',
+      classificacao: 78,
+      cartaz:'https://www.themoviedb.org/t/p/w220_and_h330_face/5BYyTmEqkYXIgdDPyEiAeAbhIsI.jpg',
+      generos:['Drama', 'Crime', 'Mistério']
+    }
+  ];
 
   constructor(public alertController: AlertController, public toastController: ToastController) {}
 
